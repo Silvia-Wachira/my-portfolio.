942b95f1b75ed1900949febc4769fa2c5a3f2d0f
Silvia-Wachira <wachirasilvia8.com>
2025-01-03 16:57:41 +0300

    create an about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 
 const About = () => {
   return (
-    <section id="about" className="py-10 px-4 bg-gray-100">
-      <h2 className="text-3xl text-center font-bold">About Me</h2>
-      <p className="mt-4 text-center">
-        I'm a software developer skilled in JavaScript, React, Python and Django . I love building
-        applications that solve real-world problems and learning new technologies.
-      </p>
-    </section>
+    // <section id="about" className="py-10 px-4 bg-gray-100">
+    //   <h2 className="text-3xl text-center font-bold">About Me</h2>
+    //   <p className="mt-4 text-center">
+    //     I'm a software developer skilled in JavaScript, React, Python and Django . I love building
+    //     applications that solve real-world problems and learning new technologies.
+    //   </p>
+    // </section>
+      <section id="about" class="bg-gradient-to-r from-cream to-almostWhite py-12">
+        <div class="max-w-4xl mx-auto text-center">
+          <h2 class="text-3xl font-bold text-coffee mb-4">About Me</h2>
+          <p class="text-grayish">I’m a developer who loves to craft intuitive and delightful user experiences.</p>
+        </div>
+      </section>
+
   );
 };
 
