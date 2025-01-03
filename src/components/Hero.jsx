80de0b29028dc35e4580c3f057b86860ac77ec9a
Silvia-Wachira <wachirasilvia8.com>
2025-01-03 16:56:49 +0300

    style the hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,13 +2,24 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <header id="home" className="text-center py-20 bg-blue-500 text-white">
-      <h1 className="text-4xl font-bold">Hi, I'm Silvia Wachira</h1>
+    <header >
+      {/* <div className="bg-coffee text-almostWhite min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Hi, I'm Silvia Wachira.</h1>
       <p className="mt-4 text-xl">A passionate developer building modern web experiences.</p>
-      <div className="mt-6">
-        <a href="https://github.com/Silvia-Wachira" className="mr-4">GitHub</a>
-        <a href="https://www.linkedin.com/in/silvia-wachira/">LinkedIn</a>
-      </div>
+        <div className="mt-6">
+          <a href="https://github.com/Silvia-Wachira" className="mr-4">GitHub</a>
+          <a href="https://www.linkedin.com/in/silvia-wachira/">LinkedIn</a>
+        </div>
+      </div> */}
+        <section class="bg-gradient-to-b from-almostWhite via-cream to-grayish text-coffee min-h-screen flex items-center justify-center">
+          <div class="text-center">
+            <h2 class="text-5xl font-bold mb-4">Hi, I'm Silvia Wachira</h2>
+            <p class="text-xl mb-6">A passionate developer creating beautiful, functional designs.</p>
+            <a href="#projects" class="bg-coffee text-almostWhite py-2 px-6 rounded shadow hover:bg-taupe">View My Work</a>
+          </div>
+        </section>
+
+
     </header>
   );
 };
