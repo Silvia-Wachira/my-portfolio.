f19e55a7c2887c30dbc8d247035ea2af94c6b212
Silvia-Wachira <wachirasilvia8.com>
2025-01-03 16:57:12 +0300

    create a contact section

---
+++ src/components/Contact.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+         <section id="contact" class="bg-gradient-to-t from-taupe via-coffee to-grayish text-almostWhite py-12">
+            <div class="max-w-4xl mx-auto text-center">
+                <h2 class="text-3xl font-bold mb-4">Get In Touch</h2>
+                <p class="mb-6">Feel free to reach out if you'd like to collaborate or just say hello!</p>
+                <a href="mailto:your-email@example.com" class="bg-almostWhite text-coffee py-2 px-6 rounded shadow hover:bg-cream">Email Me</a>
+            </div>
+        </section>
+
+    </div>
+  )
+}
+
+export default Contact
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer id="contact" className="bg-gray-800 text-white py-6 text-center">
-      <p>© 2025 [Your Name]. All rights reserved.</p>
-      <p>Contact: <a href="wachirasilvia8@gmail.com">wachirasilvia8@gmail.com</a></p>
-    </footer>
+    // <footer id="contact" className="bg-grayish text-coffee py-4 text-center">
+    //   <p>© 2025 Silvia Wachira . All rights reserved.</p>
+    //   <p>Contact: <a href="wachirasilvia8@gmail.com">wachirasilvia8@gmail.com</a></p>
+    // </footer>
+      <footer class="bg-gradient-to-r from-grayish via-taupe to-coffee text-almostWhite p-6 text-center">
+        <p>&copy; 2025 My Portfolio. All rights reserved.</p>
+      </footer>
+
   );
 };
 
