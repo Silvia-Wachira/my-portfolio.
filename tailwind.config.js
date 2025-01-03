01ff6b840cab030a8c2ded82e832314b530bda12
Silvia-Wachira <wachirasilvia8.com>
2025-01-03 16:57:27 +0300

    configure tailwind.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,38 @@
+
 // /** @type {import('tailwindcss').Config} */
 // module.exports = {
-//   content: [],
+//   content: [
+//     './src/**/*.{html,js,jsx,ts,tsx}',
+//   ],
 //   theme: {
-//     extend: {},
+//     extend: {
+//       colors: {
+//         primary: '#8A6240', 
+//         secondary: '#eab308', 
+//         accent: '#f43f5e', 
+//         neutral: '#64748b', 
+//         background: '8A6240 ',
+//         text: '#334155',
+//       },
+//     },
 //   },
 //   plugins: [],
 // }
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/**/*.{html,js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        coffee: '#654B38', // Deep Coffee
+        taupe: '#6B5B51',  // Taupe
+        grayish: '#9E9A9A', // Neutral Gray
+        cream: '#EEECDA',  // Cream
+        almostWhite: '#FAFBFA', // Almost White
+      },
+    },
   },
   plugins: [],
-}
+};
